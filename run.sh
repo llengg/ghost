c9a485ba8b41f20ca5fa03e11a1ffb7e89c6e2d6
llengg <lulengling@gmail.com>
2016-09-27 13:31:51 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-VOLUME_HOME="/data"
-G_CONF="$VOLUME_HOME/ghost/config.js"
-
 npm config set registry http://registry.cnpmjs.org
-sed -i "s/localhost/$GHOST_URL/g" /etc/nginx/conf.d/ghost.conf
 
 cd /data/ghost/
-NODE_ENV=${NODE_ENV:-production} npm start
+npm start
 
 
