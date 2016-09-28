b7642b5bc7d02260de8b767f68be9c25c1f1ebee
llengg <lulengling@gmail.com>
2016-09-28 10:41:24 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,45 @@
 #!/bin/bash
 
-npm config set registry http://registry.cnpmjs.org
+GHOST="/ghost"
+OVERRIDE="/ghost-override"
 
-cd /data/ghost/
-npm start --production
+CONFIG="config.js"
+DATA="content/data"
+IMAGES="content/images"
+THEMES="content/themes"
 
+cd "$GHOST"
+rm -rf config.js
+cp -a /config.jf ./
 
+# Symlink data directory.
+mkdir -p "$OVERRIDE/$DATA"
+rm -fr "$DATA"
+ln -s "$OVERRIDE/$DATA" "content"
+
+# Symlink images directory
+mkdir -p "$OVERRIDE/$IMAGES"
+rm -fr "$IMAGES"
+ln -s "$OVERRIDE/$IMAGES" "$IMAGES"
+
+# Symlink config file.
+if [[ -f "$OVERRIDE/$CONFIG" ]]; then
+  rm -f "$CONFIG"
+  ln -s "$OVERRIDE/$CONFIG" "$CONFIG"
+fi
+
+# Symlink themes.
+if [[ -d "$OVERRIDE/$THEMES" ]]; then
+  for theme in $(find "$OVERRIDE/$THEMES" -mindepth 1 -maxdepth 1 -type d -exec basename {} \;)
+  do
+    rm -fr "$THEMES/$theme"
+    ln -s "$OVERRIDE/$THEMES/$theme" "$THEMES/$theme"
+  done
+fi
+
+# Start Ghost
+chown -R ghost:ghost /data /ghost /ghost-override
+su ghost << EOF
+cd "$GHOST"
+NODE_ENV=${NODE_ENV:-production} npm start
+EOF
