6944f90a29e7c5dadd00c380b3368c40b43eeec3
llengg <lulengling@gmail.com>
2016-09-28 10:29:13 +0800

    Update config.js

--- config.js
+++ config.js
@@ -6,29 +6,30 @@ config = {
     production: {
         url: 'http://you_url',
         mail: {},
+       /*
         database: {
             client: 'sqlite3',
             connection: {
                 filename: path.join(__dirname, '/content/data/ghost.db')
             },
-       	/*
+       	    */
        	database: {
             client: 'mysql',
             connection: {
-                host     : 'host',
-                port     : 'port',
-                user     : 'user',
-                password : 'password',
-                database : 'database',
+                host     : '10.10.32.51',
+                port     : '3306',
+                user     : 'uaxjIcJgP5s16fQ7',
+                password : 'ppFkldAh7VuGMg04T',
+                database : 'g2jRMkpnxVoXFmAl',
                 charset  : 'utf8'
             },
-            */
+            
             debug: false
         },
 
         server: {
-            host: '127.0.0.1',
-            port: '2368'
+            host: '0.0.0.0',
+            port: '80'
         }
     }
 };
